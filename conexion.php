a10a1353268c4cc7e4fdbdc4cf54feaa04abb732
Marvin G <marvin.guerrero@hotmail.com>
2023-01-14 15:51:14 -0500

    Aplicacion sencilla php

---
+++ conexion.php
@@ -0,0 +1,38 @@
+<?php 
+
+class conexion {
+
+    private $servidor="localhost";
+    private $usuario="root";
+    private $contrasena="";
+    private $dbname = "portafolio";
+    private $conexion;
+
+    public function __construct(){
+
+        try{
+
+            $this->conexion = new PDO("mysql:host=$this->servidor;dbname=$this->dbname",$this->usuario,$this->contrasena);
+            $this->conexion->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+        }
+        catch (PDOException $e){
+            return "Falla de conexión ".$e;
+        }
+
+    }
+
+    public function ejecutar($sql){
+        $this->conexion->exec($sql);
+        return $this->conexion->lastInsertId();
+    }
+
+    public function consultar($sql){
+        $sentencia = $this->conexion->prepare($sql);
+        $sentencia->execute();
+        return $sentencia->fetchAll();
+    }
+
+}
+
+
+?>
